cc825c9e09f3970dcd7f9492218c5129eccf5743
taejin.ahn <xowls0460@gmail.com>
2024-02-01 06:17:18 +0900

    마무리 후 vercel에 배포

--- src/app/components/Memo/Editor/index.tsx
+++ src/app/components/Memo/Editor/index.tsx
@@ -22,6 +22,10 @@ const Box = styled.div`
   & .ql-container.ql-snow {
     border: 0 !important;
   }
+
+  & .ql-container > .ql-editor {
+    min-height: 50vh;
+  }
 `;
 
 const MemoDate = styled.div`
@@ -47,7 +51,9 @@ export default function MemoEditor() {
     <Box>
       <Block marginTop="5px" />
       <MemoDate>
-        {new Date(selecetedMemo?.created_at ?? '').toLocaleString()}
+        {selecetedMemo !== undefined
+          ? new Date(selecetedMemo?.created_at ?? '').toLocaleString()
+          : '새 메모를 생성해 보세요'}
       </MemoDate>
       <ReactQuill
         theme="snow"
@@ -75,6 +81,7 @@ export default function MemoEditor() {
             container: '#toolbar',
           },
         }}
+        readOnly={selecetedMemo === undefined}
         formats={['bold', 'size', 'header', 'image', 'list']}
       />
     </Box>
